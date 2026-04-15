f1bb726a09fc6bbbf3f160784cb7ccb08974b2ec
Porhotkin <porhotkin@yandex.ru>
2026-04-15 22:49:06 +0300

    fix: добавил описание  entity пользователя

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+export class User {
+  // Уникальный идентификатор пользователя
+  id!: number;
+
+  // Email пользователя (уникальный)
+  email!: string;
+
+  // Хеш пароля
+  password!: string;
+
+  // Имя пользователя (опционально)
+  name?: string;
+
+  //Дата регистрации
+  createdAt!: Date;
+
+  // Дата последнего обновления
+  updatedAt!: Date;
+}
