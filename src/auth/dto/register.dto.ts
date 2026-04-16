3d8a3179684936380d62c5f215708a319b35c13d
Dmitrii <ffurzy@gmail.com>
2026-04-16 17:37:10 -0400

    feat(auth): add register dto

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,49 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsString,
+  MinLength,
+  IsDateString,
+  IsOptional, IsArray, IsNumber
+} from "class-validator";
+
+export enum Gender {
+  MALE = 'male',
+  FEMALE = 'female',
+  OTHER = 'other',
+}
+
+export class RegisterDTO {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+
+  //необязательные поля
+  @IsOptional()
+  @IsDateString()
+  birthday?: string;
+
+  @IsOptional()
+  @IsString()
+  city?: string;
+
+  @IsOptional()
+  @IsEnum(Gender)
+  gender?: Gender;
+
+  @IsOptional()
+  @IsString()
+  about?: string;
+
+  //проверяем что массив а также что элементы являются числами
+  @IsOptional()
+  @IsArray()
+  @IsNumber({}, { each: true })
+  wantToLearn?: number[];
+}
