be23594ce9f24784bf8c70fd6fdc0b405e553634
Dmitrii <ffurzy@gmail.com>
2026-04-15 22:33:34 -0400

    fix: fix lint errors

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,7 +8,7 @@ import { AuthController } from './auth.controller';
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '15min' },
-    })
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
