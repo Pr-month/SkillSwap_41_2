2f24d0440b43004d4764745826e1ff97d307126c
WAKR <WAKR93@yandex.ru>
2026-04-17 16:47:12 +0600

    week1-wakr93-#5

--- src/auth/auth.types.ts
+++ src/auth/auth.types.ts
@@ -1,6 +1,11 @@
 import { User } from '../users/entities/user.entity';
 
-export type TJwtPayload = Pick<User, 'id' | 'email' | 'name' | 'role'>;
+
+type UserBase = Pick<User, 'email' | 'name' | 'role'>;
+
+export type TJwtPayload = UserBase & {
+  sub: number;
+};
 
 export type TRequestWithUser = Request & {
   user: TJwtPayload;
