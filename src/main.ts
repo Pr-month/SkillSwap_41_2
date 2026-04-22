d790e9c2601eab4e608aa7970fcaeab17be051ec
Najot Nafasov <nigelnafasov98@yandex.com>
2026-04-22 22:14:14 +0500

    feat: implement GET /users with database v2

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe, BadRequestException, ClassSerializerInterceptor } from '@nestjs/common';
 import { AllExceptionsFilter } from './common/all-exception.filter';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
