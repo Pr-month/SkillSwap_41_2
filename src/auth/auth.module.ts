877cfe3f6a9038933cd10c5e609739d60e7a2368
WAKR93 <WAKR93@yandex.ru>
2026-04-25 22:23:12 +0700

    week2-WAKR93-#26

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,15 +1,34 @@
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
-import { ConfigModule } from '@nestjs/config';
-import { JwtAccessStrategy } from './strategies/jwt-access.strategy';
-import { jwtConfig } from '../config/jwt.config';
-import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
 import { Module } from '@nestjs/common';
-import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User } from '../users/entities/user.entity';
+import { JwtAccessStrategy } from './strategies/jwt-access.strategy';
+import { JwtRefreshStrategy } from './strategies/jwt-refresh.strategy';
+import { jwtConfig, TJwtConfig } from '../config/jwt.config';
+import type { StringValue } from 'ms';
 
 @Module({
-  imports: [ConfigModule.forFeature(jwtConfig), UsersModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync({
+      inject: [jwtConfig.KEY],
+      useFactory: (config: TJwtConfig) => {
+        const jwt = config;
+        return {
+          secret: jwt.accessSecret,
+          signOptions: {
+            expiresIn: jwt.accessTokenExpires as StringValue,
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, JwtAccessStrategy, JwtRefreshStrategy],
+  exports: [JwtModule],
 })
 export class AuthModule {}
