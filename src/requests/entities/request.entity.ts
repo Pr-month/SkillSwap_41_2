91f8a8690fbf899a81229b7291aa86a5f1e3ca9f
Valery Kraft <WAKR93@yandex.ru>
2026-04-28 10:04:05 +0700

    week3-WAKR93-#99

--- src/requests/entities/request.entity.ts
+++ src/requests/entities/request.entity.ts
@@ -1,1 +1,54 @@
-export class Request {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Skill } from 'src/skills/entities/skill.entity';
+import { RequestStatus } from '../requests.enum';
+
+@Entity('requests')
+export class Request {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  // Время создания
+  @CreateDateColumn()
+  createdAt: Date;
+
+  // Создавший заявку юзер
+  @ManyToOne(() => User, { nullable: false })
+  sender: User;
+
+  // Юзер которому предложили
+  @ManyToOne(() => User, {
+    nullable: false,
+  })
+  receiver: User;
+
+  // status "enum список"
+  @Column({
+    type: 'enum',
+    enum: RequestStatus,
+    default: RequestStatus.Pending,
+  })
+  status: RequestStatus;
+
+  // Предложенный отправителем навык
+  @ManyToOne(() => Skill, {
+    nullable: false,
+  })
+  offeredSkill: Skill;
+
+  // Желаемый отправителем навык
+  @ManyToOne(() => Skill, {
+    nullable: false,
+  })
+  requestedSkill: Skill;
+
+  // Прочел ли получатель
+  @Column({ default: false })
+  isRead: boolean;
+}
