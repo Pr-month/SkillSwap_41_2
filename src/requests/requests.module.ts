057d94ac7a950fe4e02ead84e33c0b7df48e5381
Andrei Moskvin <andrey.mskv@gmail.com>
2026-04-28 23:32:07 +0400

    feat(requests): enable repository access for User, Skill and Request

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RequestsService } from './requests.service';
 import { RequestsController } from './requests.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { User } from '../users/entities/user.entity';
+import { Skill } from '../skills/entities/skill.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Skill, Request]), AuthModule],
   controllers: [RequestsController],
   providers: [RequestsService],
 })
