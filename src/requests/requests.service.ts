641fafa40b6ff9ed015ce0da7014831e9d1cf739
Andrei Moskvin <andrey.mskv@gmail.com>
2026-04-28 18:41:56 +0400

    feat(requests): implement request creation (POST /requests)

--- src/requests/requests.service.ts
+++ src/requests/requests.service.ts
@@ -1,11 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateRequestDto } from './dto/create-request.dto';
 import { UpdateRequestDto } from './dto/update-request.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RequestStatus } from './enums/request.enums';
+
+import { User } from 'src/users/entities/user.entity';
+import { Request } from 'src/requests/entities/request.entity';
+import { Skill } from 'src/skills/entities/skill.entity';
 
 @Injectable()
 export class RequestsService {
-  create(createRequestDto: CreateRequestDto) {
-    return 'This action adds a new request';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Request)
+    private requestsRepository: Repository<Request>,
+    @InjectRepository(Skill)
+    private skillRepository: Repository<Skill>,
+  ) {}
+
+  async create(dto: CreateRequestDto, userId: number) {
+    // Проврка существования получателя
+    const receiver = await this.userRepository.findOne({
+      where: { id: dto.receiverId },
+    });
+
+    if (!receiver) {
+      throw new NotFoundException('Получатель не найден');
+    }
+
+    // Проврка существования навыков
+    const offeredSkill = await this.skillRepository.findOne({
+      where: { id: dto.offeredSkillId },
+      relations: {
+        owner: true, // для проверки владельца
+      },
+    });
+
+    const requestedSkill = await this.skillRepository.findOne({
+      where: { id: dto.requestedSkillId },
+    });
+
+    if (!offeredSkill || !requestedSkill) {
+      throw new NotFoundException('Навык не найден');
+    }
+
+    if (offeredSkill.owner.id !== userId) {
+      throw new ForbiddenException('Нельзя предлагать чужой навык');
+    }
+
+    if (dto.receiverId === userId) {
+      throw new BadRequestException('Нельзя отправить заявку самому себе');
+    }
+
+    const request = this.requestsRepository.create({
+      sender: { id: userId },
+      receiver,
+      offeredSkill,
+      requestedSkill,
+      status: RequestStatus.PENDING,
+      isRead: false,
+    });
+
+    return this.requestsRepository.save(request);
   }
 
   findAll() {
