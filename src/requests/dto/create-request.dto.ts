d8fba06dfbabb2241ea9a737e42a13bece9403b4
Porhotkin <porhotkin@yandex.ru>
2026-05-01 22:32:19 +0300

    feat(requests): добавил CreateRequestDto

--- src/requests/dto/create-request.dto.ts
+++ src/requests/dto/create-request.dto.ts
@@ -1,1 +1,15 @@
-export class CreateRequestDto {}
+import { IsUUID, IsNotEmpty } from 'class-validator';
+
+export class CreateRequestDto {
+  @IsUUID()
+  @IsNotEmpty()
+  receiverId!: number;   // ID получателя (кому отправляем заявку)
+
+  @IsUUID()
+  @IsNotEmpty()
+  offeredSkillId!: number;   // ID навыка, который предлагает отправитель
+
+  @IsUUID()
+  @IsNotEmpty()
+  requestedSkillId!: number; // ID навыка, который хочет получить отправитель
+}
