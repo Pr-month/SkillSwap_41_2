65450c576a683708bc1ac0e637ce0914f741c442
Dmitrii <ffurzy@gmail.com>
2026-04-30 21:42:46 -0400

    feat(users): add UsersService.create method

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import {
   Injectable,
   NotFoundException,
   UnauthorizedException,
+  ConflictException
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -20,8 +21,21 @@ export class UsersService {
     private usersRepository: Repository<User>,
   ) {}
 
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async create(dto: CreateUserDto): Promise<User> {
+    const existingUser = await this.usersRepository.findOne({
+      where: { email: dto.email },
+    })
+    if (existingUser) {
+      throw new ConflictException('Пользователь с таким email уже существует');
+    }
+
+    const salt = this.configService.get<number>('app.hashSalt') ?? 10;
+    const hashedPassword = await bcrypt.hash(dto.password, salt);
+    const user = this.usersRepository.create({
+      ...dto,
+      password: hashedPassword,
+    })
+    return this.usersRepository.save(user)
   }
 
   async findAll(): Promise<User[]> {
