3149c4753a6d33e798b50a2e92fc53821d6b6134
Dmitrii <ffurzy@gmail.com>
2026-05-05 12:41:47 -0400

    feat(categories): #168 add swagger

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -14,11 +14,26 @@ import { UserRole } from 'src/users/enums/users.enums';
 import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/decorators/roles.decorator';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth, ApiParam } from '@nestjs/swagger';
 
+@ApiTags('categories')
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
+  @ApiOperation({
+    summary: 'Создание категории',
+    description: 'Создаёт новую категорию или подгатегорию',
+  })
+
+  @ApiBearerAuth()
+  @ApiResponse({ status: 201, description: 'Категория создана' })
+  @ApiResponse({ status: 400, description: 'Ошибка валидации данных' })
+  @ApiResponse({ status: 401, description: 'Пользователь не авторизован' })
+  @ApiResponse({
+    status: 403,
+    description: 'Недостаточно прав',
+  })
   @UseGuards(JwtAccessGuard, RolesGuard)
   @Roles([UserRole.ADMIN])
   @Post()
@@ -26,16 +41,45 @@ export class CategoriesController {
     return this.categoriesService.create(createCategoryDto);
   }
 
+  @ApiOperation({
+    summary: 'Получение списка категорий',
+    description: 'Возвращает все корневые категории вместе с их подкатегориями',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Список категории получен'
+  })
   @Get()
   findAll() {
     return this.categoriesService.findAll();
   }
 
+  @ApiOperation({
+    summary: 'Получение категории по ID',
+    description: 'Возвращает категорию вместе с её родителем и подкатегориями'
+  })
+  @ApiParam({ name: 'id', description: 'ID категории', example: 1 })
+  @ApiResponse({ status: 200, description: 'Категория найдена' })
+  @ApiResponse({ status: 404, description: 'Категория не найдена' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.categoriesService.findOne(+id);
   }
 
+  @ApiOperation({
+    summary: 'Обновление категории',
+    description: 'Обновляет категорию по ID',
+  })
+  @ApiBearerAuth()
+  @ApiParam({ name: 'id', description: 'ID категории', example: 1 })
+  @ApiResponse({ status: 200, description: 'Категория обновлена' })
+  @ApiResponse({ status: 400, description: 'Ошибка валидации данных' })
+  @ApiResponse({ status: 401, description: 'Пользователь не авторизован' })
+  @ApiResponse({
+    status: 403,
+    description: 'Недостаточно прав',
+  })
+  @ApiResponse({ status: 404, description: 'Категория не найдена' })
   @UseGuards(JwtAccessGuard, RolesGuard)
   @Roles([UserRole.ADMIN])
   @Patch(':id')
@@ -46,6 +90,20 @@ export class CategoriesController {
     return this.categoriesService.update(+id, updateCategoryDto);
   }
 
+
+  @ApiOperation({
+    summary: 'Удаление категории',
+    description: 'Удаляет категорию по ID',
+  })
+  @ApiBearerAuth()
+  @ApiParam({ name: 'id', description: 'ID категории', example: 1 })
+  @ApiResponse({ status: 200, description: 'Категория удалена' })
+  @ApiResponse({ status: 401, description: 'Пользователь не авторизован' })
+  @ApiResponse({
+    status: 403,
+    description: 'Недостаточно прав',
+  })
+  @ApiResponse({ status: 404, description: 'Категория не найдена' })
   @UseGuards(JwtAccessGuard, RolesGuard)
   @Roles([UserRole.ADMIN])
   @Delete(':id')
