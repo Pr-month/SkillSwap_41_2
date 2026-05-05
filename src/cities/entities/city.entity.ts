96425df8e29879e1294b5a5c2dce752998d2ec9d
Andrei Moskvin <andrey.mskv@gmail.com>
2026-05-05 09:41:36 +0400

    feat(cities): add City entity

--- src/cities/entities/city.entity.ts
+++ src/cities/entities/city.entity.ts
@@ -1,1 +1,28 @@
-export class City {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('cities')
+export class City {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  country?: string;
+
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  region?: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
