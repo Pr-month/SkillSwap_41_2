3ddeb459bc459ac224f34acea1ad6cde59977559
Smilix123 <smilix@yandex.ru>
2026-05-08 01:37:47 +0400

    feat(cities): implement CRUD logic and admin protection
    - Implement TypeORM repository logic in CitiesService for creating, finding, updating, and deleting cities.
    - Add Role-based access control to CitiesController to restrict create, update, and delete actions to administrators only.
    - Add validation decorators to CreateCityDto for data integrity.
    - Remove redundant findOne endpoint from the controller.

--- src/cities/cities.controller.ts
+++ src/cities/cities.controller.ts
@@ -1,4 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { UserRole } from 'src/users/enums/users.enums';
 import { CitiesService } from './cities.service';
 import { CreateCityDto } from './dto/create-city.dto';
 import { UpdateCityDto } from './dto/update-city.dto';
@@ -7,6 +20,8 @@ import { UpdateCityDto } from './dto/update-city.dto';
 export class CitiesController {
   constructor(private readonly citiesService: CitiesService) {}
 
+  @UseGuards(JwtAccessGuard, RolesGuard)
+  @Roles([UserRole.ADMIN])
   @Post()
   create(@Body() createCityDto: CreateCityDto) {
     return this.citiesService.create(createCityDto);
@@ -17,16 +32,15 @@ export class CitiesController {
     return this.citiesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.citiesService.findOne(+id);
-  }
-
+  @UseGuards(JwtAccessGuard, RolesGuard)
+  @Roles([UserRole.ADMIN])
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCityDto: UpdateCityDto) {
     return this.citiesService.update(+id, updateCityDto);
   }
 
+  @UseGuards(JwtAccessGuard, RolesGuard)
+  @Roles([UserRole.ADMIN])
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.citiesService.remove(+id);
--- src/cities/cities.service.ts
+++ src/cities/cities.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCityDto } from './dto/create-city.dto';
 import { UpdateCityDto } from './dto/update-city.dto';
+import { City } from './entities/city.entity';
 
 @Injectable()
 export class CitiesService {
-  create(createCityDto: CreateCityDto) {
-    return 'This action adds a new city';
+  constructor(
+    @InjectRepository(City)
+    private readonly cityRepository: Repository<City>,
+  ) {}
+
+  async create(createCityDto: CreateCityDto) {
+    const city = this.cityRepository.create({
+      name: createCityDto.name,
+      country: createCityDto.country,
+      region: createCityDto.region,
+    });
+
+    return await this.cityRepository.save(city);
   }
 
-  findAll() {
-    return `This action returns all cities`;
+  async findAll() {
+    try {
+      return this.cityRepository.find();
+    } catch (error) {
+      console.error(' Error in findAll:', error);
+      throw error;
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} city`;
+  async update(id: number, updateCityDto: UpdateCityDto) {
+    const city = await this.cityRepository.findOne({ where: { id } });
+
+    if (!city) {
+      throw new NotFoundException(`Город с ID  ${id} не найден`);
+    }
+
+    Object.assign(city, updateCityDto);
+
+    return await this.cityRepository.save(city);
   }
 
-  update(id: number, updateCityDto: UpdateCityDto) {
-    return `This action updates a #${id} city`;
-  }
+  async remove(id: number) {
+    const city = await this.cityRepository.findOne({ where: { id } });
 
-  remove(id: number) {
-    return `This action removes a #${id} city`;
+    if (!city) {
+      throw new NotFoundException(`Город с ID  ${id} не найден`);
+    }
+
+    await this.cityRepository.remove(city);
+    return { message: `Город "${city.name}" успешно удален` };
   }
 }
