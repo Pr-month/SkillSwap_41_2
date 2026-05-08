aac68741b21ab72dae0781c9e516577eb4e150a6
Dmitrii Musikhin <185406834+ffurzy@users.noreply.github.com>
2026-05-08 11:08:42 -0400

    refactor(categories): #168 split swagger to separate file

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -8,32 +8,27 @@ import {
   Delete,
   UseGuards,
 } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CategoriesService } from './categories.service';
 import { CategoryDto, UpdateCategoryDto } from './dto/category.dto';
 import { UserRole } from 'src/users/enums/users.enums';
 import { JwtAccessGuard } from 'src/auth/guards/jwt-access.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { Roles } from 'src/decorators/roles.decorator';
-import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth, ApiParam } from '@nestjs/swagger';
+import {
+  ApiCategoriesDelete,
+  ApiCategoriesGetAll,
+  ApiCategoriesGetOne,
+  ApiCategoriesPatch,
+  ApiCategoriesPost,
+} from './categories.swagger';
 
 @ApiTags('categories')
 @Controller('categories')
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) {}
 
-  @ApiOperation({
-    summary: 'Создание категории',
-    description: 'Создаёт новую категорию или подгатегорию',
-  })
-
-  @ApiBearerAuth()
-  @ApiResponse({ status: 201, description: 'Категория создана' })
-  @ApiResponse({ status: 400, description: 'Ошибка валидации данных' })
-  @ApiResponse({ status: 401, description: 'Пользователь не авторизован' })
-  @ApiResponse({
-    status: 403,
-    description: 'Недостаточно прав',
-  })
+  @ApiCategoriesPost()
   @UseGuards(JwtAccessGuard, RolesGuard)
   @Roles([UserRole.ADMIN])
   @Post()
@@ -41,45 +36,19 @@ export class CategoriesController {
     return this.categoriesService.create(createCategoryDto);
   }
 
-  @ApiOperation({
-    summary: 'Получение списка категорий',
-    description: 'Возвращает все корневые категории вместе с их подкатегориями',
-  })
-  @ApiResponse({
-    status: 200,
-    description: 'Список категории получен'
-  })
+  @ApiCategoriesGetAll()
   @Get()
   findAll() {
     return this.categoriesService.findAll();
   }
 
-  @ApiOperation({
-    summary: 'Получение категории по ID',
-    description: 'Возвращает категорию вместе с её родителем и подкатегориями'
-  })
-  @ApiParam({ name: 'id', description: 'ID категории', example: 1 })
-  @ApiResponse({ status: 200, description: 'Категория найдена' })
-  @ApiResponse({ status: 404, description: 'Категория не найдена' })
+  @ApiCategoriesGetOne()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.categoriesService.findOne(+id);
   }
 
-  @ApiOperation({
-    summary: 'Обновление категории',
-    description: 'Обновляет категорию по ID',
-  })
-  @ApiBearerAuth()
-  @ApiParam({ name: 'id', description: 'ID категории', example: 1 })
-  @ApiResponse({ status: 200, description: 'Категория обновлена' })
-  @ApiResponse({ status: 400, description: 'Ошибка валидации данных' })
-  @ApiResponse({ status: 401, description: 'Пользователь не авторизован' })
-  @ApiResponse({
-    status: 403,
-    description: 'Недостаточно прав',
-  })
-  @ApiResponse({ status: 404, description: 'Категория не найдена' })
+  @ApiCategoriesPatch()
   @UseGuards(JwtAccessGuard, RolesGuard)
   @Roles([UserRole.ADMIN])
   @Patch(':id')
@@ -90,20 +59,7 @@ export class CategoriesController {
     return this.categoriesService.update(+id, updateCategoryDto);
   }
 
-
-  @ApiOperation({
-    summary: 'Удаление категории',
-    description: 'Удаляет категорию по ID',
-  })
-  @ApiBearerAuth()
-  @ApiParam({ name: 'id', description: 'ID категории', example: 1 })
-  @ApiResponse({ status: 200, description: 'Категория удалена' })
-  @ApiResponse({ status: 401, description: 'Пользователь не авторизован' })
-  @ApiResponse({
-    status: 403,
-    description: 'Недостаточно прав',
-  })
-  @ApiResponse({ status: 404, description: 'Категория не найдена' })
+  @ApiCategoriesDelete()
   @UseGuards(JwtAccessGuard, RolesGuard)
   @Roles([UserRole.ADMIN])
   @Delete(':id')
