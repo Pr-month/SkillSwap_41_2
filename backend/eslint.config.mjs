f8bf1a3d79eca44a40c278cde4eb3e92172c1d14
Прокофьев Алексей <user@HH-NOTE-MACOS-8.local>
2026-05-19 12:06:09 +0300

    270 - fix eslint config for tests files

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -35,6 +35,11 @@ export default tseslint.config(
     files: ['**/*.spec.ts', '**/*.test.ts'],
     rules: {
       '@typescript-eslint/unbound-method': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
     },
   },
 );
