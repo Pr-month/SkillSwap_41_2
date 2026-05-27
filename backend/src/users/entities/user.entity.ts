0f293125b0a24a2d13016bc8e21c41f43bc2c99a
Andrei Moskvin <andrey.mskv@gmail.com>
2026-05-27 14:11:18 +0400

    docs(entity): add ApiProperty decorators for Swagger

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -13,15 +13,20 @@ import {
 import { Gender, UserRole } from '../enums/users.enums';
 import { City } from 'src/cities/entities/city.entity';
 import { Category } from 'src/categories/entities/category.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { expand } from 'rxjs';
 
 @Entity('users')
 export class User {
+  @ApiProperty({ example: 1234567890 })
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @ApiProperty({ example: 'John Doe' })
   @Column({ type: 'varchar', length: 255 })
   name!: string;
 
+  @ApiProperty({ example: 'john.doe@example.com' })
   @Column({ type: 'varchar', length: 255, unique: true })
   email!: string;
 
@@ -29,28 +34,36 @@ export class User {
   @Column({ type: 'varchar', length: 255 })
   password!: string;
 
+  @ApiProperty({ example: 'About me' })
   @Column({ type: 'text', nullable: true })
   about?: string;
 
+  @ApiProperty({ example: '1990-01-01' })
   @Column({ type: 'date', nullable: true })
   birthdate?: Date;
-
+  
+  @ApiProperty({ example: City })
   @ManyToOne(() => City, { nullable: true })
   city?: City;
 
+  @ApiProperty({ example: Gender })
   @Column({ nullable: true, type: 'enum', enum: Gender })
   gender?: Gender;
 
+  @ApiProperty({ example: 'https://example.com/avatar.jpg' })
   @Column({ nullable: true })
   avatar?: string;
 
+  @ApiProperty({ example: Skill })
   @OneToMany(() => Skill, (skill) => skill.owner)
   skills?: Skill[];
 
+  @ApiProperty({ example: Skill })
   @ManyToMany(() => Skill)
   @JoinTable({ name: 'user_favorite_skills' })
   favoriteSkills?: Skill[];
 
+  @ApiProperty({ example: UserRole })
   @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
   role!: UserRole;
 
@@ -58,12 +71,15 @@ export class User {
   @Column({ type: 'varchar', length: 255, nullable: true })
   refreshToken?: string | null;
 
+  @ApiProperty({ example: Request })
   @OneToMany(() => Request, (request) => request.sender)
   sentRequests!: Request[];
 
+  @ApiProperty({ example: Request })
   @OneToMany(() => Request, (request) => request.receiver)
   receivedRequests!: Request[];
 
+  @ApiProperty({ example: Category })
   @ManyToMany(() => Category, (category) => category.usersWhoWantToLearn)
   @JoinTable({
     name: 'wantToLearn',
