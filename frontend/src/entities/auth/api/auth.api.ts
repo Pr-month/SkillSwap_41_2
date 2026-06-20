c7653fd4900df181906e9834b5a910762ddc804a
khodarevski <79624036110@yandex.ru>
2026-06-20 20:38:53 +0300

    исправить эндпоинты логина/логаута и логику обновления access-токена через refresh

--- frontend/src/entities/auth/api/auth.api.ts
+++ frontend/src/entities/auth/api/auth.api.ts
@@ -2,9 +2,9 @@ import { TAuthResponse, TLoginData, TRefreshResponse } from "./auth.types";
 import { apiClient } from "@/shared/api";
 
 export const loginUserApi = async (data: TLoginData): Promise<TAuthResponse> => {
-  return apiClient.auth.post<TAuthResponse, TLoginData>('/api/login', data);
+  return apiClient.auth.post<TAuthResponse, TLoginData>('/api/auth/login', data);
 };
 
 export const logoutApi = async (): Promise<TRefreshResponse> => {
-  return apiClient.auth.post<TRefreshResponse>('/api/logout');
+  return apiClient.auth.post<TRefreshResponse>('/api/auth/logout');
 };
